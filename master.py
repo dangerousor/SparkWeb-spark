e3f8504e23136d39216d42a430ebf43bf3446046
dangerousor <1192350292@qq.com>
2019-05-04 17:36:24 +0800

    node_type

--- master.py
+++ master.py
@@ -38,7 +38,7 @@ def do_with_task(args, co, db_worker, task_id):
     res, dic, lines = task_split(args)
     data = dict()
     for each in res['in0']:
-        result, tmp = run_func(each, params=dic[each]['params'])
+        result, tmp = run_func(dic[each]['node_type'], params=dic[each]['params'])
         if not result:
             sc.cancelAllJobs()
             return False, tmp
@@ -61,7 +61,7 @@ def do_with_task(args, co, db_worker, task_id):
             return False, each + 'linked by ' + str(len(lines[each])) + 'line, need 1'
         if lines[each][0] not in data:
             return False, 'run ' + each + ' but ' + lines[each][0] + ' not in data'
-        result, tmp = run_func(each, params=dic[each]['params'], in1=data[lines[each][0]])
+        result, tmp = run_func(dic[each]['node_type'], params=dic[each]['params'], in1=data[lines[each][0]])
         if not result:
             sc.cancelAllJobs()
             return False, tmp
@@ -84,7 +84,7 @@ def do_with_task(args, co, db_worker, task_id):
             return False, each + 'linked by ' + str(len(lines[each])) + 'lines, need 2'
         if lines[each][0] not in data or lines[each][1] not in data:
             return False, 'run ' + each + ' but ' + lines[each][0] + ' or ' + lines[each][1] + ' not in data'
-        result, tmp = run_func(each, params=dic[each]['params'], in1=data[lines[each][0]], in2=data[lines[each][1]])
+        result, tmp = run_func(dic[each]['node_type'], params=dic[each]['params'], in1=data[lines[each][0]], in2=data[lines[each][1]])
         if not result:
             sc.cancelAllJobs()
             return False, tmp
